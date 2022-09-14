f6791493021012d668db5b1ea84c30007d465d89
spmackey <spmackey@ncsu.edu>
2022-09-14 16:33:50 -0400

    Modified index page

--- asgn03-static/index.php
+++ asgn03-static/index.php
@@ -11,24 +11,33 @@
 <?php 
     include 'Bird.php';
     
-    echo '<p>The instance_count for the Bird Class is '.Bird::$instance_count.'</p>';
-    echo '<p>The instance_count for the YellowBelliedFlyCatcher Class is '.YellowBelliedFlyCatcher::$instance_count.'</p>';
-    echo '<p>The instance_count for the Kiwi Class is '.Kiwi::$instance_count.'</p>';
-    $bird = Bird::create();
+    $bird = new Bird;
     echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
     echo '<p>Default egg_num value: '.Bird::$egg_num.'</p>';
 
-    $fly_catcher = YellowBelliedFlyCatcher::create();
+    $fly_catcher = new YellowBelliedFlyCatcher;
     echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
     echo '<p>'.$fly_catcher->name.' egg_num: '.YellowBelliedFlyCatcher::$egg_num.'</p>';
 
-    $kiwi = Kiwi::create();
+    $kiwi = new Kiwi;
     $kiwi->flying = "no";
     echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
     echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";   
-    echo '<p>The instance_count for the Bird Class is '.Bird::$instance_count.'</p>';
-    echo '<p>The instance_count for the YellowBelliedFlyCatcher Class is '.YellowBelliedFlyCatcher::$instance_count.'</p>';
-    echo '<p>The instance_count for the Kiwi Class is '.Kiwi::$instance_count.'</p>';
+
+    echo "<hr><h1>Static Examples</h1>";
+    echo "<h2>Before using the create method</h2>";
+    echo '<p>Bird count: '.Bird::$instance_count.'</p>';
+    echo '<p>FlyCatcher count: '.YellowBelliedFlyCatcher::$instance_count.'</p>';
+    echo '<p>Kiwi count: '.Kiwi::$instance_count.'</p>';
+
+    $bird2 = Bird::create();
+    $fly_catcher2 = YellowBelliedFlyCatcher::create();
+    $kiwi2 = Kiwi::create();
+
+    echo "<h2>After using the create method</h2>";
+    echo '<p>Bird count: '.Bird::$instance_count.'</p>';
+    echo '<p>FlyCatcher count: '.YellowBelliedFlyCatcher::$instance_count.'</p>';
+    echo '<p>Kiwi count: '.Kiwi::$instance_count.'</p>';
 
 ?>
     </body>
