f857d49053e9ff71e8676f38ef52a4124d75b702
spmackey <spmackey@ncsu.edu>
2022-09-14 15:37:14 -0400

    Completed Bird.php

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -7,14 +7,17 @@ class Bird {
     var $conservation;
     var $song = "chirp";
     var $flying = "yes";
+    public static $instance_count = 0;
+    public static $egg_num = 0;
 
     function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
+        return ( $this->flying == "yes" ) ? "can fly" : "is stuck on the ground";
+    }
+
+    public static function create() {
+      $class = get_called_class();
+      static::$instance_count++;
+      return new $class();
     }
 }
 
@@ -22,6 +25,7 @@ class YellowBelliedFlyCatcher extends Bird {
     var $name = "yellow-bellied flycatcher";
     var $diet = "mostly insects.";
     var $song = "flat chilk";
+    public static $egg_num = "3-4, sometimes 5";
 }
 
 class Kiwi extends Bird {
--- asgn03-static/index.php
+++ asgn03-static/index.php
@@ -11,16 +11,24 @@
 <?php 
     include 'Bird.php';
     
-    $bird = new Bird;
+    echo '<p>The instance_count for the Bird Class is '.Bird::$instance_count.'</p>';
+    echo '<p>The instance_count for the YellowBelliedFlyCatcher Class is '.YellowBelliedFlyCatcher::$instance_count.'</p>';
+    echo '<p>The instance_count for the Kiwi Class is '.Kiwi::$instance_count.'</p>';
+    $bird = Bird::create();
     echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
+    echo '<p>Default egg_num value: '.Bird::$egg_num.'</p>';
 
-    $fly_catcher = new YellowBelliedFlyCatcher;
+    $fly_catcher = YellowBelliedFlyCatcher::create();
     echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
+    echo '<p>'.$fly_catcher->name.' egg_num: '.YellowBelliedFlyCatcher::$egg_num.'</p>';
 
-    $kiwi = new Kiwi;
+    $kiwi = Kiwi::create();
     $kiwi->flying = "no";
     echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
-    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";    
+    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";   
+    echo '<p>The instance_count for the Bird Class is '.Bird::$instance_count.'</p>';
+    echo '<p>The instance_count for the YellowBelliedFlyCatcher Class is '.YellowBelliedFlyCatcher::$instance_count.'</p>';
+    echo '<p>The instance_count for the Kiwi Class is '.Kiwi::$instance_count.'</p>';
 
 ?>
     </body>
