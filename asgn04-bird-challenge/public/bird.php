b7475b3832e7812fe1e4c4b9e1a574475f1a3858
spmackey <spmackey@ncsu.edu>
2022-09-28 13:07:26 -0400

    Started bird-challenge

---
+++ asgn04-bird-challenge/public/bird.php
@@ -0,0 +1,53 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php $page_title = 'Inventory'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page">
+    <div class="intro">
+      <img class="inset" src="<?php echo url_for('/images/AdobeStock_55807979_thumb.jpeg') ?>" />
+      <h2>Our Inventory of Used Bicycles</h2>
+      <p>Choose the bike you love.</p>
+      <p>We will deliver it to your door and let you try it before you buy it.</p>
+    </div>
+
+    <table id="inventory">
+    <tr>
+        <th>Common Name</th>
+        <th>Habitat</th>
+        <th>Food</th>
+        <th>Nest Placement</th>
+        <th>Behavior</th>
+        <th>Conservation Level</th>
+        <th>Backyard Tips</th>
+      </tr>
+
+<?php
+
+$parser = new ParseCSV(PRIVATE_PATH . '/used_bicycles.csv');
+$bike_array = $parser->parse();
+
+
+?>
+      <?php foreach($bike_array as $args) { ?>
+        <?php $bike = new Bicycle($args); ?>
+      <tr>
+        <td><?php echo h($bike->brand); ?></td>
+        <td><?php echo h($bike->model); ?></td>
+        <td><?php echo h($bike->year); ?></td>
+        <td><?php echo h($bike->category); ?></td>
+        <td><?php echo h($bike->gender); ?></td>
+        <td><?php echo h($bike->color); ?></td>
+        <td><?php echo h($bike->weight_kg()) . ' / ' . h($bike->weight_lbs()); ?></td>
+        <td><?php echo h($bike->condition()); ?></td>
+      </tr>
+      <?php } ?>
+
+    </table>
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
