13860da923d78e39f50480595957504d7bbb280c
spmackey <spmackey@ncsu.edu>
2022-10-12 12:52:25 -0400

    Added DocBlocks

--- asgn06-fetch-all/private/classes/Bird.class.php
+++ asgn06-fetch-all/private/classes/Bird.class.php
@@ -4,10 +4,22 @@ class Bird {
 
   static public $database;
 
+  /**
+   * Sets the database used for the Bird Class
+   *
+   * @param   [mysqli]  $database  The database object to be used for the bird class
+   */
   static public function set_database($database) {
     self::$database = $database;
   }
 
+  /**
+   * A function that executes a prepared sql statement and returns an array of the results
+   *
+   * @param   [prepared statement]  $sql  A prepared sql statement that will be executed
+   *
+   * @return  [array]        An array of Bird objects from the sql statement
+   */
   static public function find_by_sql($sql) {
     $sql->execute();
     $result = $sql->get_result();
@@ -24,12 +36,24 @@ class Bird {
     return $object_array;
   }
 
+  /**
+   * Returns an array of all records in the birds table of the database
+   *
+   * @return  [array]  An array containing bird objects representing all records in the birds able
+   */
   static public function find_all() {
     $sql = self::$database->prepare("SELECT * FROM birds");
     // $sql = "SELECT * FROM birds";
     return self::find_by_sql($sql);
   }
 
+  /**
+   * Creates a Bird object from an associative array
+   *
+   * @param   [array]  $record  An associative array that corresponds with the properties of a bird object
+   *
+   * @return  [Bird]           A bird object created from the data in the record array
+   */
   static protected function instantiate($record) {
     $object = new self;
     foreach($record as $property => $value) {
@@ -39,7 +63,13 @@ class Bird {
     }
     return $object;
   }
-
+  /**
+   * Returns a bird object from the database with the selected id, or returns false if the id number does not exist.
+   *
+   * @param   [string]  $id  The id number of the bird in the database
+   *
+   * @return  [Bird]         A bird object with the corresponding id number, or false if there is no bird with that id number.
+   */
   static public function find_by_id($id) {
     $sql = self::$database->prepare("SELECT * FROM birds WHERE id = ?");
     $sql->bind_param('s', $id);
@@ -66,6 +96,13 @@ class Bird {
     4 => 'Extreme'
   ];
 
+  /**
+   * Creates a Bird object using an array of arguments
+   *
+   * @param   [array]  $args  An associative array with indexes that correspond to the properties of the Bird object
+   *
+   * @return  [Bird]             A bird object with properties set to those defined in the input array
+   */
   public function __construct($args=[])
   {
     $this->id = $args['id'] ?? NULL;
@@ -78,6 +115,11 @@ class Bird {
     $this->backyard_tips = $args['backyard_tips'] ?? NULL;  
   }
 
+  /**
+   * Returns a string that represents the conservation level of the bird object
+   *
+   * @return  [string]  The conservation level of the bird
+   */
   public function conservation_level(){
     if($this->conservation_id > 0 AND $this->conservation_id) {
       return self::CONSERVATION_LEVELS[$this->conservation_id];
