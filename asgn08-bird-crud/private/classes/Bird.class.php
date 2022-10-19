e9bf8d9921b34ac81ecbeb4a422e73e2386a7ad7
spmackey <spmackey@ncsu.edu>
2022-10-19 15:11:02 -0400

    Added Dynamic attribute list

--- asgn08-bird-crud/private/classes/Bird.class.php
+++ asgn08-bird-crud/private/classes/Bird.class.php
@@ -3,6 +3,7 @@
 class Bird {
 
   static public $database;
+  static protected $db_columns = ['id', 'common_name', 'habitat', 'food', 'conservation_id', 'backyard_tips'];
   public $errors = [];
 
   /**
@@ -84,20 +85,27 @@ class Bird {
 
   public function create() {
 
+    $attributes = $this->attributes();
     $sql = "INSERT INTO birds (";
-    $sql .= "common_name, habitat, food, conservation_id, backyard_tips";
-    $sql .= ") VALUES (";
-    $sql .= "'" . $this->common_name . "', ";
-    $sql .= "'" . $this->habitat . "', ";
-    $sql .= "'" . $this->food . "', ";
-    $sql .= "'" . $this->conservation_id . "', ";
-    $sql .= "'" . $this->backyard_tips . "'";
-    $sql .= ")";
+    $sql .= join(', ', array_keys($attributes));
+    $sql .= ") VALUES ('";
+    $sql .= join("', '", array_values($attributes));
+    $sql .= "')";
+    var_dump($sql);
     $result = self::$database->query($sql);
     if($result) {
       $this->id = self::$database->insert_id;
     }
     return $result;
+  }
+
+  public function attributes() {
+    $attributes = [];
+    foreach(static::$db_columns as $column) {
+      if($column == 'id') { continue; }
+      $attributes[$column] = $this->$column;
+    }
+    return $attributes;
   }
 
   public $id;
